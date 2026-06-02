dab1388ca2aa536200efe1e2cc39ebe63463819e
Aditi Singh <singhaditi5115@gmail.com>
2026-06-02 14:28:30 +0530

    Day 2

---
+++ week_1/Day_2/maximum_average_subarray1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int sum=0;
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+        int maxSum=sum;
+        int start=0;
+        int end=k;
+        while(end<nums.size()){
+            sum-=nums[start];
+            start++;
+
+            sum+=nums[end];
+            end++;
+            maxSum=max(maxSum,sum);
+        }
+        return (double) maxSum/k;
+    }
+};
