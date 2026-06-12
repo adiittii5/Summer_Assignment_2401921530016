bd3d98b51202b77fc4bab2228b3e1cab6757f3f1
Aditi Singh <singhaditi5115@gmail.com>
2026-06-12 23:02:00 +0530

    Week 2, Day 5

---
+++ week_2/Day_5/group_anagrams.cpp
@@ -0,0 +1,14 @@
+ unordered_map<string, vector<string>> mp;
+
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+        for (auto &p : mp) {
+            ans.push_back(p.second);
+        }
+
+        return ans;
