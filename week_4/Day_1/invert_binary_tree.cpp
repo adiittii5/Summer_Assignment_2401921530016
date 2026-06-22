7129f45b37ebf2b3b2fb264e6aad22beedc2fd94
Aditi Singh <singhaditi5115@gmail.com>
2026-06-22 21:06:37 +0530

    Week 4, Day 1

---
+++ week_4/Day_1/invert_binary_tree.cpp
@@ -0,0 +1,14 @@
+lass Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+       if (root == nullptr)
+            return nullptr;
+
+        swap(root->left, root->right);
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        return root;   
+    }
+};
