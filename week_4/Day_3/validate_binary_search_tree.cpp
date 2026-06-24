a580769bb0d9e70920ac2d0a33800715243f3a7b
Aditi Singh <singhaditi5115@gmail.com>
2026-06-24 22:56:31 +0530

    Week 4, Day 3

---
+++ week_4/Day_3/validate_binary_search_tree.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+ long long prev = LLONG_MIN;
+    bool isValidBST(TreeNode* root) {
+         if (root == nullptr) return true;
+
+        if (!isValidBST(root->left))
+            return false;
+
+        if (root->val <= prev)
+            return false;
+
+        prev = root->val;
+
+        return isValidBST(root->right);
+    }
+};
